523ad47be92619ac1d9c66272d88934dcbf69868
Apinya <Apinya@abc.com>
2019-03-18 14:28:14 +0700

    Start Data Type

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// String Type ตัวแปรที่เป็นอักษร
+
+var myName="Doramom "//นี่คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySurname: String = "AAAAAA"  //นี่คือการประกาศแบบ กำหนด Datatype  ตั้งแต่เกิด
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร
+
+var myName = "apinya"
+myName="Doramon"
+
+//Explicit Constant
+let myFriend = "Nopita"
+//myFriend ="Sunako"Error เพราะ myfriend เป็นค่าคงที่
+
+
